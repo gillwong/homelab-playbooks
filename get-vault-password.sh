7d30770fc410c45400f7ef6c3ff4f3d9f25b0caf
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2025-06-14 13:20:13 +0800

    feat: support pass for getting vault password in Linux

--- get-vault-password.sh
+++ get-vault-password.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -
-# macOS only
-/usr/bin/security find-generic-password -s homelab-playbooks -a ansible-vault -w
+
+kernel=$(uname -s)
+
+[ kernel = 'Darwin' ] && /usr/bin/security find-generic-password -s homelab-playbooks -a ansible-vault -w
+[ kernel = 'Linux' ] && /usr/bin/pass Homelab/playbooks
