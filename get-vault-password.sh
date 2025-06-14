a9e53c76f5e25000a972a97f6d938925c4a134b5
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2025-06-14 13:25:24 +0800

    fix: fix get vault pwd script

--- get-vault-password.sh
+++ get-vault-password.sh
@@ -2,5 +2,5 @@
 
 kernel=$(uname -s)
 
-[ kernel = 'Darwin' ] && /usr/bin/security find-generic-password -s homelab-playbooks -a ansible-vault -w
-[ kernel = 'Linux' ] && /usr/bin/pass Homelab/playbooks
+[ "$kernel" = 'Darwin' ] && /usr/bin/security find-generic-password -s homelab-playbooks -a ansible-vault -w
+[ "$kernel" = 'Linux' ] && /usr/bin/pass Homelab/playbooks
